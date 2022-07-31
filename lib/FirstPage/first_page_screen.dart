e41310fb11d3fdc0b33a95af8ca29babf815890d
Liliana Castellanos <lilipao140799@gmail.com>
2022-07-31 15:53:38 -0500

    up_first_page_2

--- lib/FirstPage/first_page_screen.dart
+++ lib/FirstPage/first_page_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:proj_ver1/constants.dart';
 
 class FirstPage extends StatelessWidget {
   const FirstPage({Key? key}) : super(key: key);
@@ -8,33 +9,50 @@ class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: kPrimaryColor,
       body: SafeArea(
           child: Center(
         child: Column(
           children: [
-            SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.15),
             Text(
               "UIS Wheels",
-              style: GoogleFonts.pressStart2p(),
+              style: GoogleFonts.pressStart2p(fontSize: 18),
             ),
-            ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.black, elevation: 5),
-                child: Text(
-                  "INGRESAR",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                )),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+            Text(
+              "AQUÍ VA UNA IMAGEN",
+              style: GoogleFonts.pressStart2p(fontSize: 18),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.35),
             ElevatedButton(
               onPressed: () {},
-              style:
-                  ElevatedButton.styleFrom(primary: Colors.black, elevation: 5),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+                elevation: 6,
+                fixedSize: const Size(200, 40),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              child: Text(
+                "INGRESAR",
+                style: GoogleFonts.lato(color: Colors.white),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+                elevation: 6,
+                fixedSize: const Size(200, 40),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
               child: Text(
                 "REGISTRARSE",
-                style: TextStyle(fontWeight: FontWeight.bold),
+                style: GoogleFonts.lato(color: Colors.white),
               ),
             ),
           ],
---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+
+const kPrimaryColor = Color.fromARGB(255, 150, 255, 141);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
+const kButtonPrimaryColor = Color.fromARGB(255, 0, 168, 0);
+const kButtonPrimaryLightColor = Color.fromARGB(255, 208, 223, 208);
