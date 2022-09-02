98e74d17b36bd831b045992381e61c45b28a4ffc
Liliana Castellanos <lilipao140799@gmail.com>
2022-09-01 22:41:32 -0500

    reminder

--- lib/UserRidesPage/components/selected_ride.dart
+++ lib/UserRidesPage/components/selected_ride.dart
@@ -16,27 +16,17 @@ class RidePage extends StatelessWidget {
 
 class MobileRidePage extends StatelessWidget {
   const MobileRidePage({Key? key}) : super(key: key);
-  static final bdecoration = BoxDecoration(
-    boxShadow: [
-      BoxShadow(
-        color: Colors.green.withOpacity(0.2),
-        spreadRadius: 1,
-        blurRadius: 8,
-        blurStyle: BlurStyle.inner,
-        offset: const Offset(0, 3))
-    ],
-    color: Colors.grey.withOpacity(0.7),
-    borderRadius: BorderRadius.circular(15),
-  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
+        title: const Text("Viaje"),
       ),
-      body: Row(
-
+      body: Column(
+        children: const [
+          Text("Aqui va un mapa y detalles del viaje...")
+        ]
       ),
     );
   }
