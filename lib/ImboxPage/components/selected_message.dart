3c63b4ee7bf69650e23b73a9ff1fb636ebedecb8
Liliana Castellanos <lilipao140799@gmail.com>
2022-09-02 20:42:09 -0500

    add_page

---
+++ lib/ImboxPage/components/selected_message.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:proj_ver1/responsive.dart';
+
+class MessagePage extends StatelessWidget {
+  const MessagePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Responsive(
+      mobile: MobileMessagePage(),
+    );
+  }
+}
+
+class MobileMessagePage extends StatelessWidget {
+  const MobileMessagePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("NOMBRE"),
+      ),
+      body: Column(
+        children: const [
+          Text("Aqui va el chat...")
+        ]
+      ),
+    );
+  }
+}
