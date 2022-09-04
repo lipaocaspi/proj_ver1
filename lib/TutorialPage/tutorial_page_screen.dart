148546b2a9d7fcf8b1d7b0a97ce1cf648234d1bf
Liliana Castellanos <lilipao140799@gmail.com>
2022-09-03 22:38:17 -0500

    add_features

--- lib/TutorialPage/tutorial_page_screen.dart
+++ lib/TutorialPage/tutorial_page_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:proj_ver1/responsive.dart';
+import 'package:video_player/video_player.dart';
 
 class TutorialPage extends StatelessWidget {
   const TutorialPage({
@@ -14,8 +15,31 @@ class TutorialPage extends StatelessWidget {
   }
 }
 
-class MobileTutorialPage extends StatelessWidget {
-  const MobileTutorialPage({Key? key}) : super(key: key);
+class MobileTutorialPage extends StatefulWidget {
+  const MobileTutorialPage({super.key});
+
+  @override
+  State<MobileTutorialPage> createState() => MobileTutorialPageState();
+}
+
+class MobileTutorialPageState extends State<MobileTutorialPage> {
+  late VideoPlayerController controller;
+  late Future<void> initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = VideoPlayerController.network(
+        "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4");
+    initializeVideoPlayerFuture = controller.initialize();
+    controller.setLooping(false);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +47,36 @@ class MobileTutorialPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Tutorial"),
       ),
+      // body: Container(
+        // padding: const EdgeInsets.all(15),
+        // child: FutureBuilder(
+            // future: initializeVideoPlayerFuture,
+            // builder: (context, snapshot) {
+              // if (snapshot.connectionState == ConnectionState.done) {
+                // return AspectRatio(
+                  // aspectRatio: controller.value.aspectRatio,
+                  // child: VideoPlayer(controller),
+                // );
+              // } else {
+                // return const Center(
+                  // child: CircularProgressIndicator(),
+                // );
+              // }
+            // })
+      // ),
+      // floatingActionButton: FloatingActionButton(
+        // backgroundColor: Colors.green,
+        // onPressed: () {
+          // setState(() {
+            // if (controller.value.isPlaying) {
+              // controller.pause();
+            // } else {
+              // controller.play();
+            // }
+          // });
+        // },
+        // child: Icon(controller.value.isPlaying ? Icons.pause : Icons.play_arrow)
+      // ),
     );
   }
 }
