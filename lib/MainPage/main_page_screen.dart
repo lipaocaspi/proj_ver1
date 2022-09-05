99aabf60c681acb8b898b26edc838885c05e9fed
Liliana Castellanos <lilipao140799@gmail.com>
2022-09-04 21:24:57 -0500

    up_mainpage

--- lib/MainPage/main_page_screen.dart
+++ lib/MainPage/main_page_screen.dart
@@ -32,24 +32,144 @@ class MobileMainPage extends StatelessWidget {
         title: const Text("Inicio"),
       ),
       drawer: const NavigationDrawer(),
+      // body: const MainScreen(),
     );
   }
 }
+
+// class MainScreen extends StatefulWidget {
+  // const MainScreen({Key? key}) : super(key: key);
+
+  // @override
+  // State<MainScreen> createState() => MainScreenState();
+// }
+
+// class MainScreenState extends State<MainScreen> {
+  // static const double _defaultLat = 8.85577417427599;
+  // static const double _defaultLng = 38.81151398296511;
+
+  // static const CameraPosition _defaultLocation =
+      // CameraPosition(target: LatLng(_defaultLat, _defaultLng), zoom: 15);
+
+  // late final GoogleMapController _googleMapController;
+  // MapType _currentMapType = MapType.normal;
+  // final Set<Marker> _markers = {};
+
+  // void _changeMapType() {
+    // setState(() {
+      // _currentMapType = _currentMapType == MapType.normal
+          // ? MapType.satellite
+          // : MapType.normal;
+    // });
+  // }
+
+  // void _addMarker() {
+    // setState(() {
+      // _markers.add(
+        // Marker(
+          // markerId: MarkerId('defaultLocation'),
+          // position: _defaultLocation.target,
+          // icon: BitmapDescriptor.defaultMarker,
+          // infoWindow: const InfoWindow(
+            // title: 'Really cool place',
+            // snippet: '5 Star Rating',
+          // ),
+        // ),
+      // );
+    // });
+  // }
+
+  // Future<void> _moveToNewLocation() async {
+    // const _newPosition = LatLng(40.7128, -74.0060);
+    // _googleMapController
+        // .animateCamera(CameraUpdate.newLatLngZoom(_newPosition, 15));
+    // setState(() {
+      // const marker = Marker(
+        // markerId: MarkerId('newLocation'),
+        // position: _newPosition,
+        // infoWindow: InfoWindow(title: 'New York', snippet: 'The Best Place'),
+      // );
+
+      // _markers
+        // ..clear()
+        // ..add(marker);
+    // });
+  // }
+
+  // Future<void> _goToDefaultLocation() async {
+    // const _defaultPosition = LatLng(_defaultLat, _defaultLng);
+    // _googleMapController
+        // .animateCamera(CameraUpdate.newLatLngZoom(_defaultPosition, 15));
+    // setState(() {
+      // const marker = Marker(
+        // markerId: MarkerId('My Default Location'),
+        // position: _defaultPosition,
+        // infoWindow: InfoWindow(title: 'Home', snippet: 'The Best Place'),
+      // );
+
+      // _markers
+        // ..clear()
+        // ..add(marker);
+    // });
+  // }
+  // @override
+  // Widget build(BuildContext context) => Stack(
+    // children: <Widget>[
+            // GoogleMap(
+              // onMapCreated: (controller) => _googleMapController = controller,
+              // initialCameraPosition: _defaultLocation,
+              // mapType: _currentMapType,
+              // markers: _markers,
+            // ),
+            // Container(
+              // padding: const EdgeInsets.only(top: 24, right: 12),
+              // alignment: Alignment.topRight,
+              // child: Column(
+                // children: <Widget>[
+                  // FloatingActionButton(
+                    // onPressed: _changeMapType,
+                    // backgroundColor: Colors.green,
+                    // child: const Icon(Icons.map, size: 30.0),
+                  // ),
+                  // const SizedBox(height: 20.0),
+                  // FloatingActionButton(
+                    // onPressed: _addMarker,
+                    // backgroundColor: Colors.deepPurpleAccent,
+                    // child: const Icon(Icons.add_location, size: 36.0),
+                  // ),
+                  // const SizedBox(height: 20.0),
+                  // FloatingActionButton(
+                    // onPressed: _moveToNewLocation,
+                    // backgroundColor: Colors.indigoAccent,
+                    // child: const Icon(Icons.location_city, size: 36.0),
+                  // ),
+                  // const SizedBox(height: 20.0),
+                  // FloatingActionButton(
+                    // onPressed: _goToDefaultLocation,
+                    // backgroundColor: Colors.red,
+                    // child: const Icon(Icons.home_rounded, size: 36.0),
+                  // ),
+                // ],
+              // ),
+            // ),
+          // ],
+  // );
+// }
 
 class NavigationDrawer extends StatelessWidget {
   const NavigationDrawer({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) => Drawer(
-    child: SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          buildHeader(context),
-          buildItems(context),
-        ],
-      ),
-    ),
-  );
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildHeader(context),
+              buildItems(context),
+            ],
+          ),
+        ),
+      );
 
   Widget buildHeader(BuildContext context) {
     return Material(
@@ -58,28 +178,20 @@ class NavigationDrawer extends StatelessWidget {
         onTap: () {
           Navigator.pop(context);
           Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => const UserPage()));
+              .push(MaterialPageRoute(builder: (context) => const UserPage()));
         },
         child: Container(
           padding: EdgeInsets.only(
-            top: 15 + MediaQuery.of(context).padding.top,
-            bottom: 15
-          ),
+              top: 15 + MediaQuery.of(context).padding.top, bottom: 15),
           child: Column(
             children: const [
               CircleAvatar(
                 radius: 35,
                 backgroundImage: NetworkImage(
-                  'https://images.unsplash.com/photo-1661544641467-d1811f77c71e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=650&q=80'
-                ),
+                    'https://images.unsplash.com/photo-1661544641467-d1811f77c71e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=650&q=80'),
               ),
               SizedBox(height: 10),
-              Text(
-                "Mi Perfil", 
-                style: TextStyle(
-                  fontSize: 18
-                )
-              )
+              Text("Mi Perfil", style: TextStyle(fontSize: 18))
             ],
           ),
         ),
@@ -94,77 +206,53 @@ class NavigationDrawer extends StatelessWidget {
         runSpacing: 5,
         children: [
           ListTile(
-            leading: const Icon(Icons.drive_eta),
-            title: const Text("Mis Viajes"),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const UserRidesPage()
-                )
-              );
-            }
-          ),
+              leading: const Icon(Icons.drive_eta),
+              title: const Text("Mis Viajes"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const UserRidesPage()));
+              }),
           ListTile(
-            leading: const Icon(Icons.alarm),
-            title: const Text("Viajes Programados"),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const RidesPage()
-                )
-              );
-            }
-          ),
+              leading: const Icon(Icons.alarm),
+              title: const Text("Viajes Programados"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const RidesPage()));
+              }),
           ListTile(
-            leading: const Icon(Icons.email),
-            title: const Text("Mensajes"),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const ImboxPage()
-                )
-              );
-            }
-          ),
+              leading: const Icon(Icons.email),
+              title: const Text("Mensajes"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const ImboxPage()));
+              }),
           ListTile(
-            leading: const Icon(Icons.camera),
-            title: const Text("Tutorial"),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const TutorialPage()
-                )
-              );
-            }
-          ),
+              leading: const Icon(Icons.camera),
+              title: const Text("Tutorial"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const TutorialPage()));
+              }),
           ListTile(
-            leading: const Icon(Icons.settings),
-            title: const Text("Configuración"),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const SettingsPage()
-                )
-              );
-            }
-          ),
+              leading: const Icon(Icons.settings),
+              title: const Text("Configuración"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const SettingsPage()));
+              }),
           ListTile(
-            leading: const Icon(Icons.logout),
-            title: const Text("Cerrar Sesión"),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const LoginPage()
-                )
-              );
-            }
-          ),
+              leading: const Icon(Icons.logout),
+              title: const Text("Cerrar Sesión"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const LoginPage()));
+              }),
         ],
       ),
     );
