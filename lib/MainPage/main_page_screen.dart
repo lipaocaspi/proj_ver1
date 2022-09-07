c818f3212c309d1b8da43843da4601d724352dd5
Liliana Castellanos <lilipao140799@gmail.com>
2022-09-07 12:33:16 -0500

    add_navigationbar

--- lib/MainPage/main_page_screen.dart
+++ lib/MainPage/main_page_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:proj_ver1/ImboxPage/imbox_page_screen.dart';
 import 'package:proj_ver1/RidesPage/rides_page_screen.dart';
 import 'package:proj_ver1/SettingsPage/settings_page_screen.dart';
-import 'package:proj_ver1/TutorialPage/tutorial_page_screen.dart';
+// import 'package:proj_ver1/TutorialPage/tutorial_page_screen.dart';
 import 'package:proj_ver1/UserPage/user_page_screen.dart';
 import 'package:proj_ver1/UserRidesPage/user_rides_page_screen.dart';
 import 'package:proj_ver1/constants.dart';
@@ -21,150 +21,174 @@ class MainPage extends StatelessWidget {
   }
 }
 
-class MobileMainPage extends StatelessWidget {
+class MobileMainPage extends StatefulWidget {
   const MobileMainPage({Key? key}) : super(key: key);
 
   @override
+  State<MobileMainPage> createState() => MobileMainPageState();
+}
+
+class MobileMainPageState extends State<MobileMainPage> {
+  int indexSelected = 0;
+  @override
   Widget build(BuildContext context) {
+    final children = [
+      const RidesPage(),
+      const UserRidesPage()
+    ];
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Inicio"),
+        // title: const Text("Inicio"),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+        ],
       ),
       drawer: const NavigationDrawer(),
-      // body: const MainScreen(),
+      body: children[indexSelected],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: indexSelected,
+        onTap: (index) => setState(() => indexSelected = index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.alarm),
+            label: "Viajes programados"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.drive_eta),
+            label: "Mis viajes"
+          )
+        ],
+      ),
     );
   }
 }
 
 // class MainScreen extends StatefulWidget {
-  // const MainScreen({Key? key}) : super(key: key);
+// const MainScreen({Key? key}) : super(key: key);
 
-  // @override
-  // State<MainScreen> createState() => MainScreenState();
+// @override
+// State<MainScreen> createState() => MainScreenState();
 // }
 
 // class MainScreenState extends State<MainScreen> {
-  // static const double _defaultLat = 8.85577417427599;
-  // static const double _defaultLng = 38.81151398296511;
+// static const double _defaultLat = 8.85577417427599;
+// static const double _defaultLng = 38.81151398296511;
 
-  // static const CameraPosition _defaultLocation =
-      // CameraPosition(target: LatLng(_defaultLat, _defaultLng), zoom: 15);
+// static const CameraPosition _defaultLocation =
+// CameraPosition(target: LatLng(_defaultLat, _defaultLng), zoom: 15);
 
-  // late final GoogleMapController _googleMapController;
-  // MapType _currentMapType = MapType.normal;
-  // final Set<Marker> _markers = {};
+// late final GoogleMapController _googleMapController;
+// MapType _currentMapType = MapType.normal;
+// final Set<Marker> _markers = {};
 
-  // void _changeMapType() {
-    // setState(() {
-      // _currentMapType = _currentMapType == MapType.normal
-          // ? MapType.satellite
-          // : MapType.normal;
-    // });
-  // }
+// void _changeMapType() {
+// setState(() {
+// _currentMapType = _currentMapType == MapType.normal
+// ? MapType.satellite
+// : MapType.normal;
+// });
+// }
 
-  // void _addMarker() {
-    // setState(() {
-      // _markers.add(
-        // Marker(
-          // markerId: MarkerId('defaultLocation'),
-          // position: _defaultLocation.target,
-          // icon: BitmapDescriptor.defaultMarker,
-          // infoWindow: const InfoWindow(
-            // title: 'Really cool place',
-            // snippet: '5 Star Rating',
-          // ),
-        // ),
-      // );
-    // });
-  // }
+// void _addMarker() {
+// setState(() {
+// _markers.add(
+// Marker(
+// markerId: MarkerId('defaultLocation'),
+// position: _defaultLocation.target,
+// icon: BitmapDescriptor.defaultMarker,
+// infoWindow: const InfoWindow(
+// title: 'Really cool place',
+// snippet: '5 Star Rating',
+// ),
+// ),
+// );
+// });
+// }
 
-  // Future<void> _moveToNewLocation() async {
-    // const _newPosition = LatLng(40.7128, -74.0060);
-    // _googleMapController
-        // .animateCamera(CameraUpdate.newLatLngZoom(_newPosition, 15));
-    // setState(() {
-      // const marker = Marker(
-        // markerId: MarkerId('newLocation'),
-        // position: _newPosition,
-        // infoWindow: InfoWindow(title: 'New York', snippet: 'The Best Place'),
-      // );
+// Future<void> _moveToNewLocation() async {
+// const _newPosition = LatLng(40.7128, -74.0060);
+// _googleMapController
+// .animateCamera(CameraUpdate.newLatLngZoom(_newPosition, 15));
+// setState(() {
+// const marker = Marker(
+// markerId: MarkerId('newLocation'),
+// position: _newPosition,
+// infoWindow: InfoWindow(title: 'New York', snippet: 'The Best Place'),
+// );
 
-      // _markers
-        // ..clear()
-        // ..add(marker);
-    // });
-  // }
+// _markers
+// ..clear()
+// ..add(marker);
+// });
+// }
 
-  // Future<void> _goToDefaultLocation() async {
-    // const _defaultPosition = LatLng(_defaultLat, _defaultLng);
-    // _googleMapController
-        // .animateCamera(CameraUpdate.newLatLngZoom(_defaultPosition, 15));
-    // setState(() {
-      // const marker = Marker(
-        // markerId: MarkerId('My Default Location'),
-        // position: _defaultPosition,
-        // infoWindow: InfoWindow(title: 'Home', snippet: 'The Best Place'),
-      // );
+// Future<void> _goToDefaultLocation() async {
+// const _defaultPosition = LatLng(_defaultLat, _defaultLng);
+// _googleMapController
+// .animateCamera(CameraUpdate.newLatLngZoom(_defaultPosition, 15));
+// setState(() {
+// const marker = Marker(
+// markerId: MarkerId('My Default Location'),
+// position: _defaultPosition,
+// infoWindow: InfoWindow(title: 'Home', snippet: 'The Best Place'),
+// );
 
-      // _markers
-        // ..clear()
-        // ..add(marker);
-    // });
-  // }
-  // @override
-  // Widget build(BuildContext context) => Stack(
-    // children: <Widget>[
-            // GoogleMap(
-              // onMapCreated: (controller) => _googleMapController = controller,
-              // initialCameraPosition: _defaultLocation,
-              // mapType: _currentMapType,
-              // markers: _markers,
-            // ),
-            // Container(
-              // padding: const EdgeInsets.only(top: 24, right: 12),
-              // alignment: Alignment.topRight,
-              // child: Column(
-                // children: <Widget>[
-                  // FloatingActionButton(
-                    // onPressed: _changeMapType,
-                    // backgroundColor: Colors.green,
-                    // child: const Icon(Icons.map, size: 30.0),
-                  // ),
-                  // const SizedBox(height: 20.0),
-                  // FloatingActionButton(
-                    // onPressed: _addMarker,
-                    // backgroundColor: Colors.deepPurpleAccent,
-                    // child: const Icon(Icons.add_location, size: 36.0),
-                  // ),
-                  // const SizedBox(height: 20.0),
-                  // FloatingActionButton(
-                    // onPressed: _moveToNewLocation,
-                    // backgroundColor: Colors.indigoAccent,
-                    // child: const Icon(Icons.location_city, size: 36.0),
-                  // ),
-                  // const SizedBox(height: 20.0),
-                  // FloatingActionButton(
-                    // onPressed: _goToDefaultLocation,
-                    // backgroundColor: Colors.red,
-                    // child: const Icon(Icons.home_rounded, size: 36.0),
-                  // ),
-                // ],
-              // ),
-            // ),
-          // ],
-  // );
+// _markers
+// ..clear()
+// ..add(marker);
+// });
+// }
+// @override
+// Widget build(BuildContext context) => Stack(
+// children: <Widget>[
+// GoogleMap(
+// onMapCreated: (controller) => _googleMapController = controller,
+// initialCameraPosition: _defaultLocation,
+// mapType: _currentMapType,
+// markers: _markers,
+// ),
+// Container(
+// padding: const EdgeInsets.only(top: 24, right: 12),
+// alignment: Alignment.topRight,
+// child: Column(
+// children: <Widget>[
+// FloatingActionButton(
+// onPressed: _changeMapType,
+// backgroundColor: Colors.green,
+// child: const Icon(Icons.map, size: 30.0),
+// ),
+// const SizedBox(height: 20.0),
+// FloatingActionButton(
+// onPressed: _addMarker,
+// backgroundColor: Colors.deepPurpleAccent,
+// child: const Icon(Icons.add_location, size: 36.0),
+// ),
+// const SizedBox(height: 20.0),
+// FloatingActionButton(
+// onPressed: _moveToNewLocation,
+// backgroundColor: Colors.indigoAccent,
+// child: const Icon(Icons.location_city, size: 36.0),
+// ),
+// const SizedBox(height: 20.0),
+// FloatingActionButton(
+// onPressed: _goToDefaultLocation,
+// backgroundColor: Colors.red,
+// child: const Icon(Icons.home_rounded, size: 36.0),
+// ),
+// ],
+// ),
+// ),
+// ],
+// );
 // }
 
 class NavigationDrawer extends StatelessWidget {
   const NavigationDrawer({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) => Drawer(
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.horizontal(
-        right: Radius.circular(30)
-      )
-    ),
-    backgroundColor: Colors.white,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.horizontal(right: Radius.circular(30))),
+        backgroundColor: Colors.white,
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -187,40 +211,40 @@ class NavigationDrawer extends StatelessWidget {
         },
         child: Container(
           decoration: BoxDecoration(
-            gradient: const LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomCenter,
-              colors: [
-                kPrimaryLightColor,
-                Color.fromARGB(255, 232, 248, 231)
-              ]
-            ),
-            color: kPrimaryLightColor,
-            border: Border.all(
-              width: 1,
-              color:kPrimaryLightColor
-            ),
-            borderRadius: const BorderRadius.only(bottomRight: Radius.circular(30))
-          ),
+              gradient: const LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    kPrimaryLightColor,
+                    Color.fromARGB(255, 232, 248, 231)
+                  ]),
+              color: kPrimaryLightColor,
+              border: Border.all(width: 1, color: kPrimaryLightColor),
+              borderRadius:
+                  const BorderRadius.only(bottomRight: Radius.circular(30))),
           padding: EdgeInsets.only(
-              top: 15 + MediaQuery.of(context).padding.top, bottom: 30, left: 30),
+              top: 15 + MediaQuery.of(context).padding.top,
+              bottom: 30,
+              left: 30),
           child: Row(
             children: const <Widget>[
               Expanded(
                 flex: 1,
                 // child: Align(
-                  // alignment: Alignment.center,
-                  child: Text("Liliana Paola Castellanos Pinzón", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)), 
+                // alignment: Alignment.center,
+                child: Text("Liliana Paola Castellanos Pinzón",
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
                 // ),
               ),
               Expanded(
                 // child: Align(
-                  // alignment: Alignment.centerRight,
-                  child: CircleAvatar(
-                    radius: 30,
-                    backgroundImage: NetworkImage(
-                    'https://images.unsplash.com/photo-1661544641467-d1811f77c71e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=650&q=80'),
-                  ),
+                // alignment: Alignment.centerRight,
+                child: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage(
+                      'https://images.unsplash.com/photo-1661544641467-d1811f77c71e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=650&q=80'),
+                ),
                 // )
               ),
             ],
@@ -236,22 +260,22 @@ class NavigationDrawer extends StatelessWidget {
       child: Wrap(
         runSpacing: 8,
         children: [
-          ListTile(
-              leading: const Icon(Icons.drive_eta, color: Colors.black87),
-              title: const Text("Mis Viajes"),
-              onTap: () {
+          // ListTile(
+              // leading: const Icon(Icons.drive_eta, color: Colors.black87),
+              // title: const Text("Mis Viajes"),
+              // onTap: () {
                 // Navigator.pop(context);
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const UserRidesPage()));
-              }),
-          ListTile(
-              leading: const Icon(Icons.alarm, color: Colors.black87),
-              title: const Text("Viajes Programados"),
-              onTap: () {
+                // Navigator.of(context).push(MaterialPageRoute(
+                    // builder: (context) => const UserRidesPage()));
+              // }),
+          // ListTile(
+              // leading: const Icon(Icons.alarm, color: Colors.black87),
+              // title: const Text("Viajes Programados"),
+              // onTap: () {
                 // Navigator.pop(context);
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => const RidesPage()));
-              }),
+                // Navigator.of(context).push(
+                    // MaterialPageRoute(builder: (context) => const RidesPage()));
+              // }),
           ListTile(
               leading: const Icon(Icons.email, color: Colors.black87),
               title: const Text("Mensajes"),
@@ -265,8 +289,8 @@ class NavigationDrawer extends StatelessWidget {
               title: const Text("Tutorial"),
               onTap: () {
                 // Navigator.pop(context);
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const TutorialPage()));
+                // Navigator.of(context).push(MaterialPageRoute(
+                // builder: (context) => const TutorialPage()));
               }),
           ListTile(
               leading: const Icon(Icons.settings, color: Colors.black87),
