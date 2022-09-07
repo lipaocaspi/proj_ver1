3d0226f83b50c95b8ed96a97e254313afe308b8e
Liliana Castellanos <lilipao140799@gmail.com>
2022-09-07 15:33:35 -0500

    up_color

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kBackgroundColor = Color.fromARGB(255, 150, 255, 141);
-const kPrimaryColor = Color.fromARGB(255, 74, 175, 65);
+const kPrimaryColor = Color.fromARGB(255, 87, 167, 80);
 const kPrimaryLightColor = Color.fromARGB(255, 168, 216, 164);
 const kButtonPrimaryColor = Color.fromARGB(255, 40, 170, 28);
 const kButtonPrimaryLightColor = Color.fromARGB(255, 233, 233, 233);
