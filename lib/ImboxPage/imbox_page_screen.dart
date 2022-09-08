0c183faa1dbd6b672ae944dad7c0b73827c173a0
Liliana Castellanos <lilipao140799@gmail.com>
2022-09-07 21:08:06 -0500

    up_imbox

--- lib/ImboxPage/imbox_page_screen.dart
+++ lib/ImboxPage/imbox_page_screen.dart
@@ -26,12 +26,12 @@ class MobileImboxPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-        // title: const Text("Mensajes"),
+      appBar: AppBar(
+        title: const Text("Mensajes"),
         // actions: [
           // IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
         // ],
-      // ),
+      ),
       body: Row(
         children: [
           Flexible(
