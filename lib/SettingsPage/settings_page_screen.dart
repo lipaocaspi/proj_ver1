f6ffc52089fe5fedc7dcbc55c2bf0992bc1d37c3
Liliana Castellanos <lilipao140799@gmail.com>
2022-09-07 21:07:34 -0500

    up_settings

--- lib/SettingsPage/settings_page_screen.dart
+++ lib/SettingsPage/settings_page_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:proj_ver1/LoginPage/login_page_screen.dart';
-import 'package:proj_ver1/UserPage/user_page_screen.dart';
+// import 'package:proj_ver1/TutorialPage/tutorial_page_screen.dart';
+// import 'package:proj_ver1/UserPage/user_page_screen.dart';
 import 'package:proj_ver1/constants.dart';
 import 'package:proj_ver1/responsive.dart';
 
@@ -82,32 +83,6 @@ class MobileSettingsPage extends StatelessWidget {
                         alignment: Alignment.topLeft,
                         padding: const EdgeInsets.all(10),
                         child: InkWell(
-                          onTap: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => const UserPage()));
-                          },
-                          child: Column(
-                            children: [
-                              Row(
-                                children: const <Widget>[
-                                  Expanded(child: Icon(Icons.person)),
-                                  Expanded(
-                                      flex: 4,
-                                      child: Padding(
-                                          padding: EdgeInsets.all(10),
-                                          child: Text("Mi Perfil"))),
-                                  Expanded(child: Icon(Icons.arrow_right))
-                                ],
-                              ),
-                            ],
-                          ),
-                        )),
-                    whiteBox,
-                    Container(
-                        decoration: bdecoration,
-                        alignment: Alignment.topLeft,
-                        padding: const EdgeInsets.all(10),
-                        child: InkWell(
                           // onTap: () {
                           // Navigator.of(context).push(MaterialPageRoute(
                           // builder: (context) => const MessagePage()));
@@ -181,6 +156,31 @@ class MobileSettingsPage extends StatelessWidget {
                           ),
                         )),
                     whiteBox,
+                    Container(
+                        decoration: bdecoration,
+                        alignment: Alignment.topLeft,
+                        padding: const EdgeInsets.all(10),
+                        child: InkWell(
+                          // onTap: () {
+                            // Navigator.of(context).push(MaterialPageRoute(
+                                // builder: (context) => const TutorialPage()));
+                          // },
+                          child: Column(
+                            children: [
+                              Row(
+                                children: const <Widget>[
+                                  Expanded(child: Icon(Icons.camera)),
+                                  Expanded(
+                                      flex: 4,
+                                      child: Padding(
+                                          padding: EdgeInsets.all(10),
+                                          child: Text("Tutorial"))),
+                                ],
+                              ),
+                            ],
+                          ),
+                        )),
+                    greyBox,
                     Container(
                         decoration: bdecoration,
                         alignment: Alignment.topLeft,
