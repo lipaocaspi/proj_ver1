3b907cc4f413cec82a477a70b399987a0718cf8d
Liliana Castellanos <lilipao140799@gmail.com>
2022-09-13 22:07:51 -0500

    up_login

--- lib/LoginPage/login_page_screen.dart
+++ lib/LoginPage/login_page_screen.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:form_validator/form_validator.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:proj_ver1/MainPage/main_page_screen.dart';
 import 'package:proj_ver1/SignupPage/signup_page_screen.dart';
 import 'package:proj_ver1/constants.dart';
 import 'package:proj_ver1/responsive.dart';
-
-import '../MainPage/main_page_screen.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({
@@ -21,8 +20,21 @@ class LoginPage extends StatelessWidget {
   }
 }
 
-class MobileLoginPage extends StatelessWidget {
-  const MobileLoginPage({Key? key}) : super(key: key);
+class MobileLoginPage extends StatefulWidget {
+  const MobileLoginPage({super.key});
+
+  @override
+  MobileLoginPageState createState() {
+    return MobileLoginPageState();
+  }
+}
+
+class MobileLoginPageState extends State<MobileLoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  void _validate() {
+    _formKey.currentState?.validate();  
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,132 +43,138 @@ class MobileLoginPage extends StatelessWidget {
         width: double.infinity,
         color: kBackgroundColor,
         height: MediaQuery.of(context).size.height,
-        child: Stack(
+        child: Form(
+          key: _formKey,
+          child: Stack(
           alignment: Alignment.center,
           children: <Widget>[
-            Positioned(
-              top: 25,
-              right: 10,
-              child: SizedBox(
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Navigator.of(context).push(
-                      // PageTransition(
-                        // child: const SignUpPage(),
-                        // type: PageTransitionType.fade,
-                      // ),
-                    // );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: const CircleBorder(),
-                  ),
-                  child: const Icon(Icons.person_add),
-                ),
-              ),
-            ),
             Row(
               children: [
                 Flexible(
                     child: Container(
-                        padding: EdgeInsets.all(20),
-                        child: Center(
-                          child: ListView(
+                      padding: EdgeInsets.all(10),
+                      // child: Center(
+                      child: ListView(
+                        children: [
+                          SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.09),
+                          Text(
+                            "Ingreso",
+                            style: GoogleFonts.pressStart2p(fontSize: 15),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 25),
+                          Row(
                             children: [
-                              SizedBox(
-                                height: MediaQuery.of(context).size.height * 0.09
-                              ),
-                              Text(
-                                "Ingreso",
-                                style: GoogleFonts.pressStart2p(fontSize: 15),
-                                textAlign: TextAlign.center,
-                              ),
-                              SizedBox(height: 25),
-                              Row(
-                                children: [
-                                  const Spacer(),
-                                  Expanded(
-                                    flex: 2,
-                                    child: Image.asset(
-                                      "assets/images/llanta.png",
-                                    ),
-                                  ),
-                                  const Spacer(),
-                                ],
-                              ),
-                              SizedBox(height: 25),
-                              Padding(
-                                padding: EdgeInsets.only(left: 25, right: 25),
-                                child: TextFormField(
-                                  keyboardType: TextInputType.emailAddress,
-                                  textInputAction: TextInputAction.next,
-                                  cursorColor: Colors.black,
-                                  validator: ValidationBuilder()
-                                      .maxLength(50)
-                                      .regExp(
-                                          RegExp(
-                                              r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+"),
-                                          'Ingrese un correo válido')
-                                      .build(),
-                                  decoration: const InputDecoration(
-                                    hintText: "Correo",
-                                    prefixIcon: Padding(
-                                      padding: EdgeInsets.all(10),
-                                      child: Icon(Icons.person),
-                                    ),
-                                  ),
+                              const Spacer(),
+                              Expanded(
+                                flex: 2,
+                                child: Image.asset(
+                                  "assets/images/llanta.png",
                                 ),
                               ),
-                              SizedBox(height: 15),
-                              Padding(
-                                padding: EdgeInsets.only(left: 25, right: 25),
-                                child: TextFormField(
-                                  textInputAction: TextInputAction.done,
-                                  obscureText: true,
-                                  cursorColor: Colors.black,
-                                  validator: ValidationBuilder()
-                                      .maxLength(
-                                          30, 'Número máximo de caracteres: 30')
-                                      .build(),
-                                  decoration: const InputDecoration(
-                                    hintText: "Contraseña",
-                                    prefixIcon: Padding(
-                                      padding: EdgeInsets.all(10),
-                                      child: Icon(Icons.lock),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(height: 25),
-                              Padding(
-                                padding: EdgeInsets.only(left: 50, right: 50),
-                                  child: ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.of(context).push(
-                                      PageTransition(
-                                        child: const MainPage(),
-                                        type: PageTransitionType.fade,
-                                      ),
-                                    );
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                    fixedSize: const Size(100, 45),
-                                  ),
-                                  child: Text(
-                                    "Ingresar".toUpperCase(),
-                                  ),
-                                ),
-                              ),
+                              const Spacer(),
                             ],
                           ),
-                        )))
+                          SizedBox(height: 25),
+                          Padding(
+                            padding: EdgeInsets.only(left: 35, right: 35),
+                            child: TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              textInputAction: TextInputAction.next,
+                              cursorColor: Colors.black,
+                              validator: ValidationBuilder()
+                                  .maxLength(50)
+                                  .regExp(
+                                      RegExp(
+                                          r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+"),
+                                      'Ingrese un correo válido')
+                                  .build(),
+                              decoration: const InputDecoration(
+                                hintText: "Correo",
+                                prefixIcon: Padding(
+                                  padding: EdgeInsets.all(10),
+                                  child: Icon(Icons.person),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          Padding(
+                            padding: EdgeInsets.only(left: 35, right: 35),
+                            child: TextFormField(
+                              textInputAction: TextInputAction.done,
+                              obscureText: true,
+                              cursorColor: Colors.black,
+                              validator: ValidationBuilder()
+                                  .maxLength(30, 'Número máximo de caracteres: 30')
+                                  .build(),
+                              decoration: const InputDecoration(
+                                hintText: "Contraseña",
+                                prefixIcon: Padding(
+                                  padding: EdgeInsets.all(10),
+                                  child: Icon(Icons.lock),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 25),
+                          Row(
+                            children: [
+                              Expanded(
+                                  flex: 1,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(left: 50, right: 5),
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.of(context).push(
+                                          PageTransition(
+                                            child: const SignUpPage(),
+                                            type: PageTransitionType.fade,
+                                          ),
+                                        );
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        primary: const Color.fromARGB(255, 242, 255, 239),
+                                      ),
+                                      child: Icon(Icons.person_add, color: kButtonPrimaryColor),
+                                    )
+                                  )
+                              ),
+                                Expanded(
+                                    flex: 2,
+                                    child: Padding(
+                                      padding: EdgeInsets.only(left: 5, right: 50),
+                                      child: ElevatedButton(
+                                        onPressed: () {
+                                          Navigator.of(context).push(
+                                            PageTransition(
+                                              child: const MainPage(),
+                                              type: PageTransitionType.fade,
+                                            ),
+                                          );
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                          fixedSize: const Size(100, 45),
+                                        ),
+                                        child: Text(
+                                          "Ingresar".toUpperCase(),
+                                        ),
+                                      ),
+                                    ),
+                                    )
+                                  
+                            ],
+                          ), 
+                        ],
+                      ),
+                ))
               ],
             ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.09
-            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.09),
             // SafeArea(child: child),
           ],
+        ),
         ),
       ),
     );
