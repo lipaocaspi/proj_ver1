d6fc1508f2da00786a8576514d130ecfa18496e8
Liliana Castellanos <lilipao140799@gmail.com>
2022-11-10 20:45:08 -0500

    up_settings

--- lib/SettingsPage/settings_page_screen.dart
+++ lib/SettingsPage/settings_page_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:proj_ver1/LoginPage/login_page_screen.dart';
-import 'package:proj_ver1/SettingsPage/components/help_page.dart';
+// import 'package:proj_ver1/SettingsPage/components/help_page.dart';
 import 'package:proj_ver1/SettingsPage/components/privacy_page.dart';
-import 'package:proj_ver1/SettingsPage/components/security_page.dart';
-import 'package:proj_ver1/SettingsPage/components/terms_page.dart';
+// import 'package:proj_ver1/SettingsPage/components/security_page.dart';
+// import 'package:proj_ver1/SettingsPage/components/terms_page.dart';
 import 'package:proj_ver1/constants.dart';
 import 'package:proj_ver1/responsive.dart';
 
@@ -25,57 +25,25 @@ class MobileSettingsPage extends StatelessWidget {
   const MobileSettingsPage({Key? key}) : super(key: key);
 
   static final bdecoration = BoxDecoration(
-    border: Border.all(color: Colors.black.withAlpha(130), style: BorderStyle.solid, width: 1),
+    border: Border.all(
+        color: Colors.black.withAlpha(130), style: BorderStyle.solid, width: 1),
     boxShadow: [
       BoxShadow(
-          color: Colors.green.withOpacity(0.1),
-          spreadRadius: 2,
-          blurRadius: 10,
-          blurStyle: BlurStyle.outer,
+        color: Colors.green.withOpacity(0.1),
+        spreadRadius: 2,
+        blurRadius: 10,
+        blurStyle: BlurStyle.outer,
       )
     ],
-    // color: Colors.grey.withOpacity(0.1),
     gradient: LinearGradient(
-      colors: [Colors.green.withOpacity(0.3), Colors.grey.withOpacity(0.3)],
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter
-    ),
+        colors: [Colors.green.withOpacity(0.3), Colors.grey.withOpacity(0.3)],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter),
     borderRadius: BorderRadius.circular(10),
   );
 
   @override
   Widget build(BuildContext context) {
-    Future<String?> openDialogAbout() => showDialog<String>(
-          context: context,
-          builder: (context) => AlertDialog(
-            title:
-                const Text('Acerca de UIS Wheels', textAlign: TextAlign.center),
-            content: SizedBox(
-              height: MediaQuery.of(context).size.height * 0.17,
-              child: ListView(
-                children: [
-                  Image.asset(
-                    "assets/images/llanta.png",
-                    height: 70,
-                  ),
-                  space,
-                  const Text("UIS Wheels V1",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 15,
-                      ))
-                ],
-              ),
-            ),
-            actions: [
-              TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text('CERRAR')),
-            ],
-          ),
-        );
 
     return Scaffold(
       appBar: AppBar(
@@ -86,7 +54,7 @@ class MobileSettingsPage extends StatelessWidget {
           Flexible(
             flex: 4,
             child: Container(
-              padding: EdgeInsets.all(20),
+                padding: EdgeInsets.all(20),
                 color: Colors.white,
                 child: ListView(
                   children: [
@@ -94,8 +62,8 @@ class MobileSettingsPage extends StatelessWidget {
                       children: [
                         Expanded(
                             child: Container(
-                              height: MediaQuery.of(context).size.height*0.21,
-                              decoration: bdecoration,
+                          height: MediaQuery.of(context).size.height * 0.21,
+                          decoration: bdecoration,
                           alignment: Alignment.center,
                           padding: EdgeInsets.all(15),
                           child: InkWell(
@@ -112,7 +80,9 @@ class MobileSettingsPage extends StatelessWidget {
                               children: [
                                 Icon(Icons.privacy_tip, size: 30),
                                 space,
-                                Text("Privacidad", style: TextStyle(fontSize: 20), textAlign: TextAlign.center)
+                                Text("Privacidad",
+                                    style: TextStyle(fontSize: 20),
+                                    textAlign: TextAlign.center)
                               ],
                             ),
                           ),
@@ -120,25 +90,26 @@ class MobileSettingsPage extends StatelessWidget {
                         space1,
                         Expanded(
                             child: Container(
-                              height: MediaQuery.of(context).size.height*0.21,
-                              decoration: bdecoration,
-                          // alignment: Alignment.topLeft,
+                          height: MediaQuery.of(context).size.height * 0.21,
+                          decoration: bdecoration,
                           padding: EdgeInsets.all(15),
                           child: InkWell(
                             onTap: () {
-                              Navigator.of(context).push(
-                                PageTransition(
-                                  child: const SecurityPage(),
-                                  type: PageTransitionType.fade,
-                                ),
-                              );
+                              // Navigator.of(context).push(
+                              //   PageTransition(
+                              //     child: const SecurityPage(),
+                              //     type: PageTransitionType.fade,
+                              //   ),
+                              // );
                             },
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
                                 Icon(Icons.security, size: 30),
                                 space,
-                                Text("Seguridad", style: TextStyle(fontSize: 20), textAlign: TextAlign.center)
+                                Text("Seguridad",
+                                    style: TextStyle(fontSize: 20),
+                                    textAlign: TextAlign.center)
                               ],
                             ),
                           ),
@@ -150,25 +121,26 @@ class MobileSettingsPage extends StatelessWidget {
                       children: [
                         Expanded(
                             child: Container(
-                              height: MediaQuery.of(context).size.height*0.21,
-                              decoration: bdecoration,
-                          // alignment: Alignment.topLeft,
+                          height: MediaQuery.of(context).size.height * 0.21,
+                          decoration: bdecoration,
                           padding: EdgeInsets.all(15),
                           child: InkWell(
                             onTap: () {
-                              Navigator.of(context).push(
-                                PageTransition(
-                                  child: const TermsPage(),
-                                  type: PageTransitionType.fade,
-                                ),
-                              );
+                              // Navigator.of(context).push(
+                              //   PageTransition(
+                              //     child: const TermsPage(),
+                              //     type: PageTransitionType.fade,
+                              //   ),
+                              // );
                             },
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
                                 Icon(Icons.document_scanner, size: 30),
                                 space,
-                                Text("Términos y Condiciones", style: TextStyle(fontSize: 20), textAlign: TextAlign.center)
+                                Text("Términos y Condiciones",
+                                    style: TextStyle(fontSize: 20),
+                                    textAlign: TextAlign.center)
                               ],
                             ),
                           ),
@@ -180,25 +152,26 @@ class MobileSettingsPage extends StatelessWidget {
                       children: [
                         Expanded(
                             child: Container(
-                              height: MediaQuery.of(context).size.height*0.21,
-                              decoration: bdecoration,
-                          // alignment: Alignment.topLeft,
+                          height: MediaQuery.of(context).size.height * 0.21,
+                          decoration: bdecoration,
                           padding: EdgeInsets.all(15),
                           child: InkWell(
                             onTap: () {
-                              Navigator.of(context).push(
-                                PageTransition(
-                                  child: const HelpPage(),
-                                  type: PageTransitionType.fade,
-                                ),
-                              );
+                              // Navigator.of(context).push(
+                              //   PageTransition(
+                              //     child: const HelpPage(),
+                              //     type: PageTransitionType.fade,
+                              //   ),
+                              // );
                             },
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
                                 Icon(Icons.help, size: 30),
                                 space,
-                                Text("Ayuda", style: TextStyle(fontSize: 20), textAlign: TextAlign.center)
+                                Text("Ayuda",
+                                    style: TextStyle(fontSize: 20),
+                                    textAlign: TextAlign.center)
                               ],
                             ),
                           ),
@@ -206,20 +179,55 @@ class MobileSettingsPage extends StatelessWidget {
                         space1,
                         Expanded(
                             child: Container(
-                              height: MediaQuery.of(context).size.height*0.21,
-                              decoration: bdecoration,
-                          // alignment: Alignment.topLeft,
+                          height: MediaQuery.of(context).size.height * 0.21,
+                          decoration: bdecoration,
                           padding: EdgeInsets.all(15),
                           child: InkWell(
                             onTap: () {
-                              openDialogAbout();
+                              showModalBottomSheet(
+                                context: context,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(15),
+                                    topRight: Radius.circular(15)
+                                  )
+                                ),
+                                builder: (context) {
+                                  return Padding (
+                                    padding: EdgeInsets.all(25),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Text("Acerca de UIS Wheels",
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                            ),),
+                                        doublespace,
+                                        Image.asset(
+                                          "assets/images/llanta.png",
+                                          height: 70,
+                                        ),
+                                        doublespace,
+                                        const Text(
+                                          "UIS Wheels V1",
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                          ))
+                                      ]
+                                    )
+                                  );
+                                });
+                              // openDialogAbout();
                             },
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
                                 Icon(Icons.info, size: 30),
                                 space,
-                                Text("Acerca de UIS Wheels", style: TextStyle(fontSize: 20), textAlign: TextAlign.center)
+                                Text("Acerca de UIS Wheels",
+                                    style: TextStyle(fontSize: 20),
+                                    textAlign: TextAlign.center)
                               ],
                             ),
                           ),
@@ -230,26 +238,76 @@ class MobileSettingsPage extends StatelessWidget {
                     Padding(
                       padding: EdgeInsets.only(left: 60, right: 60),
                       child: Container(
-                        // height: 130,
-                        // width: 130,
                         decoration: bdecoration,
-                        // alignment: Alignment.topLeft,
                         padding: EdgeInsets.all(10),
                         child: InkWell(
                           onTap: () {
-                            Navigator.of(context).push(
-                              PageTransition(
-                                  child: const LoginPage(),
-                                  type: PageTransitionType.fade,
-                                ),
-                            );
+                            showModalBottomSheet(
+                              context: context,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(15),
+                                  topRight: Radius.circular(15)
+                                )
+                              ),
+                              builder: (context) {
+                                return Padding (
+                                  padding: EdgeInsets.all(25),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text("¿Desea cerrar sesión?",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                        ),),
+                                      doublespace,
+                                      Row(
+                                        children: [
+                                          Expanded(
+                                            child: Padding(
+                                                padding: EdgeInsets.only(
+                                                    left: 8, right: 8),
+                                                child: ElevatedButton(
+                                                    onPressed: () {
+                                                      Navigator.pop(context);
+                                                    },
+                                                    style: ElevatedButton
+                                                        .styleFrom(
+                                                            primary:
+                                                                Colors.grey),
+                                                    child: Text("CANCELAR"))),
+                                          ),
+                                          Expanded(
+                                            child: Padding(
+                                                padding: EdgeInsets.only(
+                                                    left: 8, right: 8),
+                                                child: ElevatedButton(
+                                                  onPressed: () {
+                                                    Navigator.of(context).push(MaterialPageRoute(
+                                                      builder: (context) => const LoginPage()));
+                                                  },
+                                                  style:
+                                                      ElevatedButton.styleFrom(
+                                                          primary:
+                                                              Colors.red),
+                                                  child: Text("CERRAR SESIÓN"),
+                                                )),
+                                          )
+                                        ],
+                                      ),
+                                    ],
+                                  )
+                                );
+                              });
                           },
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Icon(Icons.logout, size: 30),
                               space1,
-                              Text("Cerrar Sesión", style: TextStyle(fontSize: 15), textAlign: TextAlign.center)
+                              Text("Cerrar Sesión",
+                                  style: TextStyle(fontSize: 15),
+                                  textAlign: TextAlign.center)
                             ],
                           ),
                         ),
