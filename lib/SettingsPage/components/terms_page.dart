c29bc7353c95f1d995f590dbd88dd3f01f24a935
Liliana Castellanos <lilipao140799@gmail.com>
2022-11-11 15:34:51 -0500

    up_layout

--- lib/SettingsPage/components/terms_page.dart
+++ lib/SettingsPage/components/terms_page.dart
@@ -23,11 +23,63 @@ class MobileTermsPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Términos y Condiciones"),
       ),
-      body: Column(
-        children: const [
-          Text("...")
-        ]
-      ),
+      body: Column(children: [
+        Container(
+          padding: EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 15),
+          child: InkWell(
+            onTap: () {},
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Icon(Icons.document_scanner),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 4,
+                      child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child:
+                            Text("Términos y condiciones de uso de UIS Wheels"),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 15, right: 15, bottom: 10),
+          child: InkWell(
+            onTap: () {},
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Icon(Icons.privacy_tip),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 4,
+                      child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Text("Aviso de privacidad de UIS Wheels"),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
