ddb6c241482e5674865f0fd3cac961eca6c45abd
Liliana Castellanos <lilipao140799@gmail.com>
2022-11-10 20:45:30 -0500

    up_constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,4 +8,5 @@ const kButtonPrimaryLightColor = Color.fromARGB(255, 233, 233, 233);
 const kIconColor = Color.fromARGB(255, 168, 216, 164);
 const kTextColor = Color.fromARGB(255, 0, 0, 0);
 const space = SizedBox(height: 10);
+const doublespace = SizedBox(height: 20);
 const space1 = SizedBox(width: 10);
