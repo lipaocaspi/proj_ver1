5ed7d1bd17923720ed11a30e2a39965d0b648a09
Liliana Castellanos <lilipao140799@gmail.com>
2022-11-11 20:32:22 -0500

    up_securitypage

--- lib/SettingsPage/components/security_page.dart
+++ lib/SettingsPage/components/security_page.dart
@@ -23,11 +23,42 @@ class MobileSecurityPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Seguridad"),
       ),
-      body: Column(
-        children: const [
-          Text("...")
-        ]
-      ),
+      body: Column(children: [
+        Container(
+          padding: EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 15),
+          child: InkWell(
+            onTap: () {},
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Icon(Icons.keyboard),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 4,
+                      child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child:
+                            Text("PIN de Seguridad"),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Icon(Icons.keyboard_arrow_right)
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
