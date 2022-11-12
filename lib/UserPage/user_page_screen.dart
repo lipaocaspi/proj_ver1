c2fb7a8acefdc1f70e23a258d81e9641dcf32698
Liliana Castellanos <lilipao140799@gmail.com>
2022-11-12 15:59:33 -0500

    up_validation

--- lib/UserPage/user_page_screen.dart
+++ lib/UserPage/user_page_screen.dart
@@ -6,6 +6,7 @@ import 'package:proj_ver1/constants.dart';
 import 'package:proj_ver1/responsive.dart';
 // import 'package:fluttertoast/fluttertoast.dart';
 import 'package:proj_ver1/variables.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class UserPage extends StatelessWidget {
   const UserPage({
@@ -86,164 +87,166 @@ class MobileUserPageState extends State<MobileUserPage> {
           Flexible(
               flex: 4,
               child: Container(
-                  padding: EdgeInsets.only(top: 15, bottom: 15, left: 30, right: 30),
-                      child: Form(
-                          // key: _formKey,
-                          child: ListView(
-                            children: <Widget>[
-                              Stack(alignment: Alignment.center, children: [
-                                CircleAvatar(
-                                  radius: 57,
-                                  backgroundColor: Colors.black,
-                                  child: CircleAvatar(
-                                    radius: 55,
-                                    backgroundImage: NetworkImage(
-                                        'https://images.unsplash.com/photo-1661544641467-d1811f77c71e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=650&q=80'),
-                                  ),
-                                ),
-                                Positioned(
-                                    right: MediaQuery.of(context).size.width *
-                                        0.25,
-                                    bottom: 5,
-                                    child: Container(
-                                      child: Padding(
-                                          padding: const EdgeInsets.all(2),
-                                          child: Icon(Icons.edit, size: 20)),
-                                      decoration: BoxDecoration(
-                                          border: Border.all(
-                                            width: 3,
-                                            color: Colors.white,
-                                          ),
-                                          borderRadius: BorderRadius.all(
-                                              Radius.circular(50)),
-                                          color: Colors.white,
-                                          boxShadow: [
-                                            BoxShadow(
-                                              offset: Offset(2, 4),
-                                              color:
-                                                  Colors.black.withOpacity(0.3),
-                                              blurRadius: 3,
-                                            )
-                                          ]),
-                                    ))
-                              ]),
-                              SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      0.04),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    child: Padding(
-                                        padding: const EdgeInsets.all(2),
-                                        child: Icon(Icons.drive_eta, size: 30)),
-                                    decoration: icondecoration,
-                                  ),
-                                  SizedBox(
-                                      width: MediaQuery.of(context).size.width *
-                                          0.06),
-                                  Container(
-                                    child: Padding(
-                                        padding: const EdgeInsets.all(2),
-                                        child: Icon(Icons.face, size: 30)),
-                                    decoration: icondecoration,
-                                  ),
-                                  SizedBox(
-                                      width: MediaQuery.of(context).size.width *
-                                          0.06),
-                                  Container(
-                                    child: Padding(
-                                        padding: const EdgeInsets.all(2),
-                                        child: Icon(Icons.star, size: 30)),
-                                    decoration: icondecoration,
-                                  ),
-                                ],
-                              ),
-                              SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      0.04),
-                              TextFormField(
-                                textInputAction: TextInputAction.next,
-                                style: TextStyle(fontSize: 18),
-                                cursorColor: Colors.black,
-                                decoration: const InputDecoration(
-                                  prefixIcon: Padding(
-                                    padding: EdgeInsets.all(10),
-                                    child: Icon(Icons.person),
-                                  ),
-                                ),
-                                // validator: ValidationBuilder()
-                                //     .maxLength(
-                                //         50, 'Número máximo de carácteres: 50')
-                                //     .build(),
-                                controller:
-                                    TextEditingController(text: '${name}'),
-                              ),
-                              space,
-                              TextFormField(
-                                onTap: () async {
-                                  DateTime? newDate = await showDatePicker(
-                                      context: context,
-                                      initialDate: DateTime.now(),
-                                      firstDate: DateTime(1900),
-                                      lastDate: DateTime(2100));
-                                  if (newDate == null) return;
-                                  setState(() => date = newDate);
+                  padding:
+                      EdgeInsets.only(top: 15, bottom: 15, left: 30, right: 30),
+                  child: Form(
+                      // key: _formKey,
+                      child: ListView(
+                    children: <Widget>[
+                      Stack(alignment: Alignment.center, children: [
+                        CircleAvatar(
+                          radius: 57,
+                          backgroundColor: Colors.black,
+                          child: CircleAvatar(
+                            radius: 55,
+                            backgroundImage: NetworkImage(
+                                'https://images.unsplash.com/photo-1661544641467-d1811f77c71e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=650&q=80'),
+                          ),
+                        ),
+                        Positioned(
+                            right: MediaQuery.of(context).size.width * 0.25,
+                            bottom: 5,
+                            child: InkWell(
+                                onTap: () {
+                                  showBarModalBottomSheet(
+                                    expand: true,
+                                    context: context, 
+                                    builder: (context){
+                                      return Padding(
+                                        padding: EdgeInsets.all(20)
+                                      );
+                                    });
                                 },
-                                textInputAction: TextInputAction.next,
-                                cursorColor: Colors.black,
-                                decoration: const InputDecoration(
-                                  prefixIcon: Padding(
-                                    padding: EdgeInsets.all(10),
-                                    child: Icon(Icons.calendar_month),
-                                  ),
-                                ),
-                                // validator: ValidationBuilder()
-                                //     .regExp(
-                                //         RegExp(
-                                //             r"^([0-9]|[1-2][0-9]|(3)[0-1])(\/)(([0-9])|((1)[0-2]))(\/)\d{4}"),
-                                //         "Ingrese una fecha válida")
-                                //     .build(),
-                                controller: TextEditingController(
-                                    text:
-                                        '${date.day}/${date.month}/${date.year}'),
-                              ),
-                              space,
-                              TextFormField(
-                                keyboardType: TextInputType.emailAddress,
-                                textInputAction: TextInputAction.done,
-                                cursorColor: Colors.black,
-                                decoration: const InputDecoration(
-                                  prefixIcon: Padding(
-                                    padding: EdgeInsets.all(10),
-                                    child: Icon(Icons.email),
-                                  ),
-                                ),
-                                // validator: (email) => email != null &&
-                                //         !EmailValidator.validate(email)
-                                //     ? 'Ingrese un correo válido'
-                                //     : null,
-                                controller:
-                                    TextEditingController(text: '${email}'),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(
-                                    left: 60, right: 60, top: 10),
-                                child: ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                      fixedSize: const Size(250, 35)),
-                                  onPressed: () {
-                                    // final isValidForm =
-                                    //     _formKey.currentState!.validate();
-                                    // if (isValidForm) {
-                                    //   showSToast();
-                                    // }
-                                  },
-                                  child: const Icon(Icons.save),
-                                ),
-                              ),
-                            ],
-                          )))),
+                                child: Container(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(2),
+                                    child: Icon(Icons.edit, size: 20)),
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      width: 3,
+                                      color: Colors.white,
+                                    ),
+                                    borderRadius:
+                                      BorderRadius.all(Radius.circular(50)),
+                                    color: Colors.white,
+                                    boxShadow: [
+                                      BoxShadow(
+                                        offset: Offset(2, 4),
+                                        color: Colors.black.withOpacity(0.3),
+                                        blurRadius: 3,
+                                      )
+                                    ]),
+                                )))
+                      ]),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.04),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            child: Padding(
+                                padding: const EdgeInsets.all(2),
+                                child: Icon(Icons.drive_eta, size: 30)),
+                            decoration: icondecoration,
+                          ),
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.06),
+                          Container(
+                            child: Padding(
+                                padding: const EdgeInsets.all(2),
+                                child: Icon(Icons.face, size: 30)),
+                            decoration: icondecoration,
+                          ),
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.06),
+                          Container(
+                            child: Padding(
+                                padding: const EdgeInsets.all(2),
+                                child: Icon(Icons.star, size: 30)),
+                            decoration: icondecoration,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.04),
+                      TextFormField(
+                        textInputAction: TextInputAction.next,
+                        style: TextStyle(fontSize: 18),
+                        cursorColor: Colors.black,
+                        decoration: const InputDecoration(
+                          prefixIcon: Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Icon(Icons.person),
+                          ),
+                        ),
+                        // validator: ValidationBuilder()
+                        //     .maxLength(
+                        //         50, 'Número máximo de carácteres: 50')
+                        //     .build(),
+                        controller: TextEditingController(text: '${name}'),
+                      ),
+                      space,
+                      TextFormField(
+                        onTap: () async {
+                          DateTime? newDate = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(1900),
+                              lastDate: DateTime(2100));
+                          if (newDate == null) return;
+                          setState(() => date = newDate);
+                        },
+                        textInputAction: TextInputAction.next,
+                        cursorColor: Colors.black,
+                        decoration: const InputDecoration(
+                          prefixIcon: Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Icon(Icons.calendar_month),
+                          ),
+                        ),
+                        // validator: ValidationBuilder()
+                        //     .regExp(
+                        //         RegExp(
+                        //             r"^([0-9]|[1-2][0-9]|(3)[0-1])(\/)(([0-9])|((1)[0-2]))(\/)\d{4}"),
+                        //         "Ingrese una fecha válida")
+                        //     .build(),
+                        controller: TextEditingController(
+                            text: '${date.day}/${date.month}/${date.year}'),
+                      ),
+                      space,
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        textInputAction: TextInputAction.done,
+                        cursorColor: Colors.black,
+                        decoration: const InputDecoration(
+                          prefixIcon: Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Icon(Icons.email),
+                          ),
+                        ),
+                        // validator: (email) => email != null &&
+                        //         !EmailValidator.validate(email)
+                        //     ? 'Ingrese un correo válido'
+                        //     : null,
+                        controller: TextEditingController(text: '${email}'),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 60, right: 60, top: 10),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              fixedSize: const Size(250, 35)),
+                          onPressed: () {
+                            // final isValidForm =
+                            //     _formKey.currentState!.validate();
+                            // if (isValidForm) {
+                            //   showSToast();
+                            // }
+                          },
+                          child: const Icon(Icons.save),
+                        ),
+                      ),
+                    ],
+                  )))),
         ],
       ),
     );
