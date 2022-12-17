2e019fe1beddf5a428ccbb58bb0e58c99599b705
Liliana Castellanos <lilipao140799@gmail.com>
2022-12-16 22:32:00 -0500

    up_firstpage

--- lib/FirstPage/first_page_screen.dart
+++ lib/FirstPage/first_page_screen.dart
@@ -2,33 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:proj_ver1/FirstPage/components/image.dart';
 import 'package:proj_ver1/FirstPage/components/signup_login_btn.dart';
 import 'package:proj_ver1/components/background.dart';
-import 'package:proj_ver1/data/repository/models/user_model.dart';
-import 'package:proj_ver1/responsive.dart';
 
 class FirstPage extends StatelessWidget {
-  const FirstPage({Key? key}) : super(key: key);
+  FirstPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Background(
-      child: SingleChildScrollView(
-        child: SafeArea(
-          child: Responsive(
-            mobile: MobileFirstPage(),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MobileFirstPage extends StatelessWidget {
-  MobileFirstPage({Key? key}) : super(key: key);
-  final List<Users> _users = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
+      child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         const Imag(),
@@ -43,6 +24,7 @@ class MobileFirstPage extends StatelessWidget {
           ],
         ),
       ],
-    );
+    )
+    ); 
   }
 }
