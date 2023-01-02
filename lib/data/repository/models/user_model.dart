91fb4bbce6896b3747ab06d614b81035f5fb9abd
Liliana Castellanos <lilipao140799@gmail.com>
2023-01-02 18:49:50 -0500

    up_usermodel

--- lib/data/repository/models/user_model.dart
+++ lib/data/repository/models/user_model.dart
@@ -7,32 +7,37 @@ String usersToJson(List<Users> data) =>
     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Users {
-  Users(
-      {required this.id,
-      required this.name,
-      required this.email,
-      required this.dateOfBirth,
-      required this.password,
+  Users({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.dateOfBirth,
+    required this.icon,
+    required this.password,
   });
 
   int id;
   String name;
   String email;
   String dateOfBirth;
+  String icon;
   String password;
 
   factory Users.fromJson(Map<String, dynamic> json) => Users(
-      id: json["id"],
-      name: json["name"],
-      email: json["email"],
-      dateOfBirth: json["dateOfBirth"],
-      password: json["password"],);
+        id: json["id"],
+        name: json["name"],
+        email: json["email"],
+        dateOfBirth: json["dateOfBirth"],
+        icon: json["icon"],
+        password: json["password"],
+      );
 
   Map<String, dynamic> toJson() => {
         "id": id,
         "name": name,
         "email": email,
         "dateOfBirth": dateOfBirth,
+        "icon": icon,
         "password": password,
       };
 }
