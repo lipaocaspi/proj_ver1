3cffa220fbef2e88d9a6ab6e8db32b29fec9d175
Liliana Castellanos <lilipao140799@gmail.com>
2023-01-02 20:37:52 -0500

    up_securitypage

--- lib/SettingsPage/components/security_page.dart
+++ lib/SettingsPage/components/security_page.dart
@@ -1,21 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:proj_ver1/responsive.dart';
 
 class SecurityPage extends StatelessWidget {
-  const SecurityPage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Responsive(
-      mobile: MobileSecurityPage(),
-    );
-  }
-}
-
-class MobileSecurityPage extends StatelessWidget {
-  const MobileSecurityPage({Key? key}) : super(key: key);
+  const SecurityPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,42 +9,43 @@ class MobileSecurityPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Seguridad"),
       ),
-      body: Column(children: [
-        Container(
-          padding: EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 15),
-          child: InkWell(
-            onTap: () {},
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.all(5),
-                        child: Icon(Icons.keyboard),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 15),
+            child: InkWell(
+              onTap: () {},
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Icon(Icons.keyboard),
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 4,
-                      child: Padding(
-                        padding: EdgeInsets.all(5),
-                        child:
-                            Text("PIN de Seguridad"),
+                      Expanded(
+                        flex: 4,
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Text("PIN de Seguridad"),
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.all(5),
-                        child: Icon(Icons.keyboard_arrow_right)
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Icon(Icons.keyboard_arrow_right)
+                        ),
                       ),
-                    ),
-                  ],
-                )
-              ],
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-      ]),
+        ]
+      ),
     );
   }
 }
