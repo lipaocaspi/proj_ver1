b0be727a74666a7d9b38b5aea8798e1f16800350
Liliana Castellanos <lilipao140799@gmail.com>
2023-01-02 20:36:12 -0500

    up_termspage

--- lib/SettingsPage/components/terms_page.dart
+++ lib/SettingsPage/components/terms_page.dart
@@ -1,21 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:proj_ver1/responsive.dart';
 
 class TermsPage extends StatelessWidget {
-  const TermsPage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Responsive(
-      mobile: MobileTermsPage(),
-    );
-  }
-}
-
-class MobileTermsPage extends StatelessWidget {
-  const MobileTermsPage({Key? key}) : super(key: key);
+  const TermsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,75 +9,77 @@ class MobileTermsPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Términos y Condiciones"),
       ),
-      body: Column(children: [
-        Container(
-          padding: EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 15),
-          child: InkWell(
-            onTap: () {},
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.all(5),
-                        child: Icon(Icons.document_scanner),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 15),
+            child: InkWell(
+              onTap: () {},
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Icon(Icons.document_scanner),
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 4,
-                      child: Padding(
-                        padding: EdgeInsets.all(5),
-                        child:
+                      Expanded(
+                        flex: 4,
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child:
                             Text("Términos y condiciones de uso de UIS Wheels"),
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.all(5),
-                        child: Icon(Icons.keyboard_arrow_right)
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Icon(Icons.keyboard_arrow_right)
+                        ),
                       ),
-                    ),
-                  ],
-                )
-              ],
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.only(left: 15, right: 15, bottom: 10),
-          child: InkWell(
-            onTap: () {},
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.all(5),
-                        child: Icon(Icons.privacy_tip),
+          Container(
+            padding: EdgeInsets.only(left: 15, right: 15, bottom: 10),
+            child: InkWell(
+              onTap: () {},
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Icon(Icons.privacy_tip),
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 4,
-                      child: Padding(
-                        padding: EdgeInsets.all(5),
-                        child: Text("Aviso de privacidad de UIS Wheels"),
+                      Expanded(
+                        flex: 4,
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Text("Aviso de privacidad de UIS Wheels"),
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.all(5),
-                        child: Icon(Icons.keyboard_arrow_right)
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Icon(Icons.keyboard_arrow_right)
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        )
-      ]),
+          )
+        ]
+      ),
     );
   }
 }
