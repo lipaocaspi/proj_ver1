28e8bb80ee44f82a3b3435c2c81fae3d566f25d2
Liliana Castellanos <lilipao140799@gmail.com>
2023-01-05 10:41:36 -0500

    up_main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:proj_ver1/FirstPage/components/bloc/user_bloc.dart';
-import 'package:proj_ver1/FirstPage/first_page_screen.dart';
-import 'package:proj_ver1/RidesPage/components/bloc/ride_bloc.dart';
 import 'package:proj_ver1/constants.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:proj_ver1/data/ride_repository.dart';
 import 'package:proj_ver1/data/user_repository.dart';
+import 'package:proj_ver1/FirstPage/first_page_screen.dart';
+import 'package:proj_ver1/FirstPage/components/bloc/user_bloc.dart';
+import 'package:proj_ver1/RidesPage/components/bloc/ride_bloc.dart';
 
 class SimpleBlocObserver extends BlocObserver {
   @override
@@ -32,56 +32,50 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(providers: [
-      BlocProvider<UserBloc>(
-        create: (context) => UserBloc(repository: UsersRepositoryImpl())
-          ..add(InitialUserEvent()),
-      ),
-      BlocProvider<RideBloc>(
-        create: (context) =>
-            RideBloc(repository: RideRepositoryImpl())..add(InitialRideEvent()),
-      ),
-    ],
-    child: MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'UIS Wheels',
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-            color: kPrimaryColor,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<UserBloc>(
+          create: (context) => UserBloc(repository: UsersRepositoryImpl())..add(InitialUserEvent()),
+        ),
+        BlocProvider<RideBloc>(
+          create: (context) => RideBloc(repository: RideRepositoryImpl())..add(InitialRideEvent()),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'UIS Wheels',
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
             centerTitle: true,
+            color: kPrimaryColor,
             foregroundColor: Colors.white
-        ),
-        primaryColor: Colors.green.withOpacity(0.8),
-        scaffoldBackgroundColor: Color.fromARGB(255, 255, 254, 254),
-        textTheme: GoogleFonts.latoTextTheme(),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            primary: kButtonPrimaryColor,
-            elevation: 4,
-            textStyle: const TextStyle(fontWeight: FontWeight.bold),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
           ),
-        ),
-        textSelectionTheme: TextSelectionThemeData(
-          cursorColor: Colors.black
-        ),
-        inputDecorationTheme: InputDecorationTheme(
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kButtonPrimaryColor,
+              elevation: 4,
+              textStyle: const TextStyle(fontWeight: FontWeight.bold),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            ),
+          ),
+          inputDecorationTheme: InputDecorationTheme(
             filled: true,
             fillColor: Color.fromARGB(255, 253, 255, 253),
             iconColor: Colors.white,
-            prefixIconColor:
-                Color.fromARGB(255, 131, 131, 131).withOpacity(0.8),
-            contentPadding:
-                const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            prefixIconColor: Color.fromARGB(255, 131, 131, 131).withOpacity(0.8),
+            contentPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
             border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(7),
-                borderSide:
-                    BorderSide(style: BorderStyle.solid, color: Colors.black))
+              borderRadius: BorderRadius.circular(7),
+              borderSide: BorderSide(style: BorderStyle.solid, color: Colors.black)
+            )
+          ),
+          primaryColor: Colors.green.withOpacity(0.8),
+          scaffoldBackgroundColor: Color.fromARGB(255, 255, 254, 254),
+          textTheme: GoogleFonts.latoTextTheme(),
+          textSelectionTheme: TextSelectionThemeData(cursorColor: Colors.black),
         ),
-      ),
-      home: FirstPage(),
-    )
+        home: FirstPage(),
+      )
     );
   }
 }
